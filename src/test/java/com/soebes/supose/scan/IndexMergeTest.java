81e71a46a7084f2b7050f9d7eb96bfec5474d183
kama <kama@2a5690f4-4b40-0410-ba62-85a317ce182b>
2007-12-25 21:09:05 +0000

    - Fixed #44   - Removed directories which had been created by the tests. - Updated links on Maven project page.
    git-svn-id: http://svn.serverix/supose/trunk@74 2a5690f4-4b40-0410-ba62-85a317ce182b

--- src/test/java/com/soebes/supose/scan/IndexMergeTest.java
+++ src/test/java/com/soebes/supose/scan/IndexMergeTest.java
@@ -1,15 +1,46 @@
 package com.soebes.supose.scan;
 
+import java.io.File;
+
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.store.FSDirectory;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
 
 public class IndexMergeTest {
 
+	static public boolean deleteDirectory(String fileName) {
+		File f = new File(fileName);
+		return deleteDirectory(f);
+	}
+
+	static public boolean deleteDirectory(File path) {
+		if (path.exists()) {
+			File[] files = path.listFiles();
+			for (int i = 0; i < files.length; i++) {
+				if (files[i].isDirectory()) {
+					deleteDirectory(files[i]);
+				} else {
+					files[i].delete();
+				}
+			}
+		}
+		return (path.delete());
+	}
+
 	private static void addTokenizedField(Document doc, String fieldName, String value) {
 		doc.add(new Field(fieldName,  value, Field.Store.YES, Field.Index.TOKENIZED));
+	}
+
+	@AfterClass
+	public void afterClass() {
+		assertTrue(deleteDirectory("index1"), "Something wrong during deletion of index1");
+		assertTrue(deleteDirectory("index2"), "Something wrong during deletion of index2");
+		assertTrue(deleteDirectory("result"), "Something wrong during deletion of result");
 	}
 
 	//1. Create index with some contents
