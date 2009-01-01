ab211e419cacbc5de30a58073e3e847a06ff0273
kama <kama@2a5690f4-4b40-0410-ba62-85a317ce182b>
2009-01-01 15:26:20 +0000

    - Fixed #134   - Updated License Header in files   - Updated License Header PlugIn Revision in POM file.
    git-svn-id: http://svn.serverix/supose/trunk@143 2a5690f4-4b40-0410-ba62-85a317ce182b

--- src/main/java/com/soebes/supose/index/Index.java
+++ src/main/java/com/soebes/supose/index/Index.java
@@ -1,9 +1,9 @@
-/*
+/**
  * The (S)ubversion Re(po)sitory (S)earch (E)ngine (SupoSE for short).
  *
- * Copyright (c) 2007, 2008 by SoftwareEntwicklung Beratung Schulung (SoEBeS)
- * Copyright (c) 2007, 2008 by Karl Heinz Marbaise
-
+ * Copyright (c) 2007, 2008, 2009 by SoftwareEntwicklung Beratung Schulung (SoEBeS)
+ * Copyright (C) 2007, 2008, 2009 by Karl Heinz Marbaise
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -16,12 +16,11 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA *
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
  *
  * The License can viewed online under http://www.gnu.org/licenses/gpl.html
  * If you have any questions about the Software or about the license
  * just write an email to license@soebes.de
- *
  */
 // SupoSE
 package com.soebes.supose.index;
