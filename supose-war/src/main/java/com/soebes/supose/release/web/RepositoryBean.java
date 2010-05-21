4241b683201d1ff7c65c56b8d64af9534cfd86a8
kama <kama@2a5690f4-4b40-0410-ba62-85a317ce182b>
2010-05-21 17:41:01 +0000

    - refs #315   Added supose-war area.
    git-svn-id: http://svn.serverix/supose/branches/B_0.7.1@488 2a5690f4-4b40-0410-ba62-85a317ce182b

---
+++ supose-war/src/main/java/com/soebes/supose/release/web/RepositoryBean.java
@@ -0,0 +1,41 @@
+package com.soebes.supose.release.web;
+
+public class RepositoryBean {
+	private String index;
+	
+	private String url;
+	private String urlviewvc;
+
+	public void setIndex(String index) {
+		this.index = index;
+	}
+
+	public String getIndex() {
+		return index;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getUrl() {
+		if (url.endsWith("/")) {
+			return url;
+		} else {
+			return url + "/";
+		}
+	}
+
+	public void setUrlviewvc(String urlviewvc) {
+		this.urlviewvc = urlviewvc;
+	}
+
+	public String getUrlviewvc() {
+		if (urlviewvc.endsWith("/")) {
+			return urlviewvc;
+		} else {
+			return urlviewvc + "/";
+		}
+	}
+
+}
---
+++ supose-war/src/main/java/com/soebes/supose/release/web/TreeHandler.java
@@ -0,0 +1,77 @@
+/**
+ * 
+ * @author Karl Heinz Marbaise
+ */
+package com.soebes.supose.release.web;
+
+import javax.faces.component.UIComponent;
+import javax.faces.event.AbortProcessingException;
+import javax.faces.event.ActionEvent;
+
+import org.apache.log4j.Logger;
+import org.apache.myfaces.custom.tree2.HtmlTree;
+import org.apache.myfaces.custom.tree2.TreeNode;
+
+/**
+ * Managed bean for the tree page
+ * @author Karl Heinz Marbaise
+ *
+ */
+public class TreeHandler {
+	private static Logger log = Logger.getLogger(TreeHandler.class);
+
+	private TreeModel treeModel;
+
+	private String reposRelativePath;
+	private String folder;
+	private String revisionStr;
+
+    public TreeHandler() {
+    	log.debug("TreeHandler::TreeHandler()");
+    }
+
+    public TreeModel getTreeModel() {
+        return treeModel;
+    }
+
+    public TreeNode getRoot() {
+    	log.debug("TreeHandler::getRoot()");
+    	return null;
+    }
+
+    public String expandAll() {
+    	treeModel.expandAll();
+    	return null;    	
+    }
+
+    public void processAction(ActionEvent event) throws AbortProcessingException {
+    	log.debug("TreeHandler::processAction()");
+        UIComponent component = (UIComponent)event.getSource();
+        while (!(component != null && component instanceof HtmlTree)) {
+            component = component.getParent();
+        }
+        if (component != null) {
+            HtmlTree tree = (HtmlTree)component;
+            tree.setNodeSelected(event);
+        }
+    }
+    
+    public String getRepository() {
+    	return reposRelativePath;
+	}
+
+	public String getFolder() {
+		return folder;
+	}
+
+	public String getRevision() {
+		return revisionStr;
+	}
+
+
+	public String ok() {	
+    	log.info("ok()");
+
+		return "SUCCESS";
+	}
+}
