b59f32c88a14ebe8b04f10bcc530f594606c127a
kama <kama@2a5690f4-4b40-0410-ba62-85a317ce182b>
2010-09-18 14:30:49 +0000

    - refs #327   - Changed the unit test to handle other cases as well.   - Added filenameInclude, propertyInclude and pathInclude as well.
    git-svn-id: http://svn.serverix/supose/branches/B_327@546 2a5690f4-4b40-0410-ba62-85a317ce182b

--- supose-filter/src/main/java/com/soebes/supose/config/filter/Filtering.java
+++ supose-filter/src/main/java/com/soebes/supose/config/filter/Filtering.java
@@ -28,6 +28,12 @@ import com.soebes.supose.config.filter.model.Filter;
 import com.soebes.supose.config.filter.model.Repository;
 
 /**
+ * This will handle the filtering of artifacts (filenames,
+ * paths and properties) during the scan process.
+ * 
+ * You can define the repository id which is used to search 
+ * in the configuration file.
+ * 
  * @author Karl Heinz Marbaise
  *
  */
@@ -46,6 +52,11 @@ public class Filtering {
 	public Filtering(Filter filter) {
 		setFilter(filter);
 		setRepositoryId(DEFAULT_REPOSITORY);
+	}
+
+	public Filtering(Filter filter, String repositoryId) {
+		setFilter(filter);
+		setRepositoryId(repositoryId);
 	}
 
 	public String getRepositoryId() {
@@ -69,6 +80,14 @@ public class Filtering {
 		return getFilter().getRepository(getRepositoryId());
 	}
 
+	/**
+	 * This method will check if the given filename 
+	 * should be ignored based on the given configuration.
+	 * 
+	 * @param fileName The name you would like to check if ignored or not.
+	 * @return true if the given filename has to be ignored false
+	 * 	otherwise.
+	 */
 	public boolean ignoreFilename(String fileName) {
 		boolean result = false;
 		if (getRepository().getFilenames().hasIncludes()) {
@@ -90,7 +109,14 @@ public class Filtering {
 		return result;
 	}
 
-	public boolean ignorePaths(String pathName) {
+	/**
+	 * This method will check if the given
+	 * path should be ignored or not.
+	 * 
+	 * @param pathName The path which will be checked.
+	 * @return true if the given path will be ignored false otherwise.
+	 */
+	public boolean ignorePath(String pathName) {
 		boolean result = false;
 		if (getRepository().getPaths().hasIncludes()) {
 			for(String regex : getRepository().getPaths().getIncludes()) {
@@ -110,4 +136,103 @@ public class Filtering {
 
 		return result;
 	}
+
+	public boolean ignoreProperty(String propertyName) {
+		boolean result = false;
+		if (getRepository().getProperties().hasIncludes()) {
+			for(String regex : getRepository().getProperties().getIncludes()) {
+				if (propertyName.matches(regex)) {
+					result = false;
+				}
+			}
+		}
+
+		if (getRepository().getPaths().hasExcludes()) {
+			for(String regex : getRepository().getProperties().getExcludes()) {
+				if (propertyName.matches(regex)) {
+					result = true;
+				}
+			}
+		}
+
+		return result;
+	}
+
+	/**
+	 * This will check if the given filename matches
+	 * the include list or not.
+	 * 
+	 * @param fileName The filename which will be check if included or not.
+	 * @return true if filename matches the include list or false otherwise.
+	 */
+	public boolean includeFilename(String fileName) {
+		boolean result = false;
+		if (getRepository().getFilenames().hasIncludes()) {
+			for(String regex : getRepository().getFilenames().getIncludes()) {
+				if (fileName.matches(regex)) {
+					result = true;
+				}
+			}
+		}
+		return result;
+	}
+
+	public boolean includePath(String pathName) {
+		boolean result = false;
+		if (getRepository().getPaths().hasIncludes()) {
+			for(String regex : getRepository().getPaths().getIncludes()) {
+				if (pathName.matches(regex)) {
+					result = true;
+				}
+			}
+		}
+		return result;
+	}
+
+	public boolean includeProperty(String propertyName) {
+		boolean result = false;
+		if (getRepository().getProperties().hasIncludes()) {
+			for(String regex : getRepository().getProperties().getIncludes()) {
+				if (propertyName.matches(regex)) {
+					result = true;
+				}
+			}
+		}
+		return result;
+	}
+	
+	public boolean excludeFilename(String fileName) {
+		boolean result = false;
+		if (getRepository().getFilenames().hasExcludes()) {
+			for(String regex : getRepository().getFilenames().getExcludes()) {
+				if (fileName.matches(regex)) {
+					result = true;
+				}
+			}
+		}
+		return result;
+	}
+	public boolean excludePath(String pathName) {
+		boolean result = false;
+		if (getRepository().getPaths().hasExcludes()) {
+			for(String regex : getRepository().getPaths().getExcludes()) {
+				if (pathName.matches(regex)) {
+					result = true;
+				}
+			}
+		}
+		return result;
+	}
+	public boolean excludeProperty(String propertyName) {
+		boolean result = false;
+		if (getRepository().getProperties().hasExcludes()) {
+			for(String regex : getRepository().getProperties().getExcludes()) {
+				if (propertyName.matches(regex)) {
+					result = true;
+				}
+			}
+		}
+		return result;
+	}
+
 }
--- supose-filter/src/test/java/com/soebes/supose/config/filter/FilteringWithExcludeTest.java
+++ supose-filter/src/test/java/com/soebes/supose/config/filter/FilteringWithExcludeTest.java
@@ -30,7 +30,6 @@ import java.io.IOException;
 
 import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.soebes.supose.TestBase;
@@ -40,54 +39,19 @@ import com.soebes.supose.config.filter.model.Filter;
  * @author Karl Heinz Marbaise
  *
  */
-public class FilteringTest extends TestBase {
+public class FilteringWithExcludeTest extends TestBase {
 
-	private Filtering filter = null;
-	
-	@BeforeClass
-	public void beforeClass() throws FileNotFoundException, IOException, XmlPullParserException {
-		filter = getFiltering();
-	}
-
-	public Filtering getFiltering() throws FileNotFoundException, IOException, XmlPullParserException {
-    	File filterFile = new File(getTestResourcesDirectory() + File.separatorChar + "filter.xml");
+	public Filtering getFilteringWithExcludes() throws FileNotFoundException, IOException, XmlPullParserException {
+    	File filterFile = new File(getTestResourcesDirectory() + File.separatorChar + "filter-with-excludes.xml");
     	Filter filterConfiguration = FilterFile.getFilter(filterFile);
 		Filtering filtering = new Filtering(filterConfiguration);
 		return filtering;
 	}
 
 	@Test
-	public void filenameIgnoreTest() throws FileNotFoundException, IOException, XmlPullParserException {
+	public void filenamesIncludeWithExcludeTest() throws FileNotFoundException, IOException, XmlPullParserException {
 		String fileName = "test.pas";
-		Assert.assertEquals(filter.ignoreFilename(fileName), false);
-	}
-	@Test
-	public void filenameIncludeTest() throws FileNotFoundException, IOException, XmlPullParserException {
-		String fileName = "test.pas";
-		Assert.assertEquals(filter.includeFilename(fileName), true);
-	}
-	
-	@Test
-	public void pathIgnoreTest() throws FileNotFoundException, IOException, XmlPullParserException {
-		String path = "/trunk";
-		Assert.assertEquals(filter.ignorePath(path), false);
-	}
-
-	@Test
-	public void pathIncludeTest() throws FileNotFoundException, IOException, XmlPullParserException {
-		String path = "/trunk";
-		Assert.assertEquals(filter.includePath(path), true);
-	}
-
-	@Test
-	public void propertyIgnoreTest() throws FileNotFoundException, IOException, XmlPullParserException {
-		String property = "svn:executable";
-		Assert.assertEquals(filter.ignoreProperty(property), false);
-	}
-
-	@Test
-	public void propertyIncludeTest() throws FileNotFoundException, IOException, XmlPullParserException {
-		String property = "svn:ignore";
-		Assert.assertEquals(filter.includeProperty(property), true);
+		Filtering filter = getFilteringWithExcludes();
+		Assert.assertEquals(filter.ignoreFilename(fileName), true);
 	}
 }
