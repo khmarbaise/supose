e599d5ffd319843ccfee260e3061fb7a5b44d8af
Karl Heinz Marbaise <info@soebes.de>
2011-11-12 14:20:11 +0100

    - Cleaned up.

--- supose-filter/filter/src/test/java/com/soebes/supose/config/filter/FilteringTest.java
+++ supose-filter/filter/src/test/java/com/soebes/supose/config/filter/FilteringTest.java
@@ -1,23 +1,23 @@
 /**
  * The (Su)bversion Re(po)sitory (S)earch (E)ngine (SupoSE for short).
- *
+ * 
  * Copyright (c) 2007, 2008, 2009, 2010 by SoftwareEntwicklung Beratung Schulung (SoEBeS)
  * Copyright (c) 2007, 2008, 2009, 2010 by Karl Heinz Marbaise
- *
+ * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
- *
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ * 
  * The License can viewed online under http://www.gnu.org/licenses/gpl.html
  * If you have any questions about the Software or about the license
  * just write an email to license@soebes.de
@@ -38,56 +38,57 @@ import com.soebes.supose.config.filter.model.Filter;
 
 /**
  * @author Karl Heinz Marbaise
- *
+ * 
  */
 public class FilteringTest extends TestBase {
 
-	private Filtering filter = null;
-	
-	@BeforeClass
-	public void beforeClass() throws FileNotFoundException, IOException, XmlPullParserException {
-		filter = getFiltering();
-	}
+    private Filtering filter = null;
 
-	public Filtering getFiltering() throws FileNotFoundException, IOException, XmlPullParserException {
-    	File filterFile = new File(getTestResourcesDirectory() + File.separatorChar + "filter.xml");
-    	Filter filterConfiguration = FilterFile.getFilter(filterFile);
-		Filtering filtering = new Filtering(filterConfiguration);
-		return filtering;
-	}
+    @BeforeClass
+    public void beforeClass() throws FileNotFoundException, IOException, XmlPullParserException {
+        filter = getFiltering();
+    }
 
-	@Test
-	public void filenameIgnoreTest() throws FileNotFoundException, IOException, XmlPullParserException {
-		String fileName = "test.pas";
-		Assert.assertEquals(filter.ignoreFilename(fileName), false);
-	}
-	@Test
-	public void filenameIncludeTest() throws FileNotFoundException, IOException, XmlPullParserException {
-		String fileName = "test.pas";
-		Assert.assertEquals(filter.includeFilename(fileName), true);
-	}
-	
-	@Test
-	public void pathIgnoreTest() throws FileNotFoundException, IOException, XmlPullParserException {
-		String path = "/trunk";
-		Assert.assertEquals(filter.ignorePath(path), false);
-	}
+    public Filtering getFiltering() throws FileNotFoundException, IOException, XmlPullParserException {
+        File filterFile = new File(getTestResourcesDirectory() + File.separatorChar + "filter.xml");
+        Filter filterConfiguration = FilterFile.getFilter(filterFile);
+        Filtering filtering = new Filtering(filterConfiguration);
+        return filtering;
+    }
 
-	@Test
-	public void pathIncludeTest() throws FileNotFoundException, IOException, XmlPullParserException {
-		String path = "/trunk";
-		Assert.assertEquals(filter.includePath(path), true);
-	}
+    @Test
+    public void filenameIgnoreTest() throws FileNotFoundException, IOException, XmlPullParserException {
+        String fileName = "test.pas";
+        Assert.assertEquals(filter.ignoreFilename(fileName), false);
+    }
 
-	@Test
-	public void propertyIgnoreTest() throws FileNotFoundException, IOException, XmlPullParserException {
-		String property = "svn:executable";
-		Assert.assertEquals(filter.ignoreProperty(property), false);
-	}
+    @Test
+    public void filenameIncludeTest() throws FileNotFoundException, IOException, XmlPullParserException {
+        String fileName = "test.pas";
+        Assert.assertEquals(filter.includeFilename(fileName), true);
+    }
 
-	@Test
-	public void propertyIncludeTest() throws FileNotFoundException, IOException, XmlPullParserException {
-		String property = "svn:ignore";
-		Assert.assertEquals(filter.includeProperty(property), true);
-	}
+    @Test
+    public void pathIgnoreTest() throws FileNotFoundException, IOException, XmlPullParserException {
+        String path = "/trunk";
+        Assert.assertEquals(filter.ignorePath(path), false);
+    }
+
+    @Test
+    public void pathIncludeTest() throws FileNotFoundException, IOException, XmlPullParserException {
+        String path = "/trunk";
+        Assert.assertEquals(filter.includePath(path), true);
+    }
+
+    @Test
+    public void propertyIgnoreTest() throws FileNotFoundException, IOException, XmlPullParserException {
+        String property = "svn:executable";
+        Assert.assertEquals(filter.ignoreProperty(property), false);
+    }
+
+    @Test
+    public void propertyIncludeTest() throws FileNotFoundException, IOException, XmlPullParserException {
+        String property = "svn:ignore";
+        Assert.assertEquals(filter.includeProperty(property), true);
+    }
 }
